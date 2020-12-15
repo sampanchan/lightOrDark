bb38039188a390f1c07de8bc15e7156940fab3f6
sampanchan <spankratz@my.creativecircus.edu>
2020-12-14 20:34:20 -0500

    saber graphs

--- js/index.js
+++ js/index.js
@@ -3,6 +3,147 @@ console.log('hello')
 let vader = document.querySelector('.badDad')
 let yoda = document.querySelector('.jediGoblin')
 
+// saber percentages
+
+let saber = document.querySelector('.beam')
+// let saberBeamers = document.querySelector('.beamers')
+let vaderVote = 0
+let yodaVote = 0
+
+
+// EVERYTHING
+let getVotes = function() {
+    // when the page loads, display the current vote counts
+    axios.get('http://circuslabs.net:3000/data/yoda').then(function (response) {
+        console.log('here is the response data for key:', response);
+        let yodaVoteSpan = document.querySelector('.yodaVotes')
+        yodaVote = response.data.data.value
+        yodaVoteSpan.innerHTML = yodaVote
+        yodaBar()
+    })
+    axios.get('http://circuslabs.net:3000/data/vader').then(function (response) {
+        console.log('here is the response data for key:', response);
+        let vaderVoteSpan = document.querySelector('.vaderVotes')
+        vaderVote = response.data.data.value
+        vaderVoteSpan.innerHTML = vaderVote
+        vaderBar()
+       
+    })
+  
+
+}
+
+getVotes()
+setInterval(getVotes, 2000)
+
+
+
+
+// percentage bars
+let vaderBar = function(){
+    saber.style.width = vaderVote/ (yodaVote+vaderVote) *100 +'%'
+    if (vaderVote === yodaVote){
+        saber.style.width = '50%'
+    }
+}
+
+let yodaBar = function(){
+    saber.style.wdith = yodaVote/ (vaderVote+yodaVote) *100 + '%'
+    if (yodaVote === vaderVote){
+        saber.style.width = '50%'
+    }
+}
+
+
+
+
+// clicks and votes
+vader.addEventListener('click', function(){
+    console.log('i am dad')
+    vader.style.borderColor = "red"
+    
+    vader.style.borderRadius ="50px"
+    vader.style.padding = "10px"
+    vader.style.boxShadow = "-10px 20px 30px red"
+    yoda.style.borderColor = ""
+    yoda.style.borderRadius = ""
+    yoda.style.padding = ""
+    yoda.style.boxShadow = ""
+    
+    axios.post('http://circuslabs.net:3000/data/vader', {
+        type: 'number',
+        action: '++',
+    }).then(getVotes)
+
+   
+})
+
+
+
+
+yoda.addEventListener('click', function(){
+    console.log('yoda i am')
+    yoda.style.borderColor = "green"
+    yoda.style.borderRadius ="50px"
+    yoda.style.padding = "10px"
+    yoda.style.boxShadow = "10px 20px 30px green"
+    vader.style.borderColor = ""
+    vader.style.borderColor = ""
+    vader.style.borderRadius = ""
+    vader.style.padding = ""
+    vader.style.boxShadow = ""
+
+
+    axios.post('http://circuslabs.net:3000/data/yoda', {
+        type: 'number',
+        action: '++',
+    }).then(getVotes)
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+// let vVotes = vaderVoteSpan
+// let yVotes= yodaVoteSpan
+
+
+// let voteTotal = vVotes + yVotes;
+// let vPercent = vVotes / voteTotal
+
+// vaderBeam.style.width = 200 * percent + "px"
+
+// vVotes = parseInt()
+
+
+
+// let updateVotes = function(){
+
+//     let vPercent = vVotes / voteTotal;
+//     let yPercent = yVotes / voteTotal;
+  
+//     vaderBeam.style.width = 200 * percent + "px";
+//     yodaBeam.style.width = 200 * percent + "px";
+//   }
+
+
+//   let vaderUpdate = function(){
+//     vaderSaber.style.width = "80px"
+// }
+// vaderUpdate()
+
+
+
+
+
 
 
 
@@ -10,70 +151,17 @@ let yoda = document.querySelector('.jediGoblin')
 // http://circuslabs.net:3000/data/starwars7
 
 
+// let vaderUrl = 'http://circuslabs.net:3000/data/vader'
+// let yodaUrl = 'http://circuslabs.net:3000/data/yoda'
 
-let getVotes = function() {
-    // when the page loads, display the current vote counts
-    axios.get('http://circuslabs.net:3000/data/yoda').then(function (response) {
-        console.log('here is the response data for key:', response);
-        let yodaVoteSpan = document.querySelector('.yodaVotes')
-        yodaVoteSpan.innerHTML = response.data.data.value
-    })
-    axios.get('http://circuslabs.net:3000/data/vader').then(function (response) {
-        console.log('here is the response data for key:', response);
-        let vaderVoteSpan = document.querySelector('.vaderVotes')
-        vaderVoteSpan.innerHTML = response.data.data.value
-    })
-}
-getVotes()
-setInterval(getVotes, 2000)
-
-
-vader.addEventListener('click', function(){
-    console.log('i am dad')
-    vader.style.borderColor = "red"
-    yoda.style.borderColor = ""
-    
-    
-    axios.post('http://circuslabs.net:3000/data/vader', {
-        type: 'number',
-        action: '++',
-    }).then(getVotes)
-    
-
-    
-})
+// const yodaVote = axios.get(yodaUrl).response.data.data.value
+// const vaderVote = axios.get(vaderUrl).response.data.data.value
 
 
 
-yoda.addEventListener('click', function(){
-    console.log('yoda i am')
-    yoda.style.borderColor = "green"
-    vader.style.borderColor = ""
 
 
 
-    // setTimeout(function() {
-    //     yoda.style.borderColor = ""
-    // }, 500)
-
-    
-    axios.post('http://circuslabs.net:3000/data/yoda', {
-        type: 'number',
-        action: '++',
-    }).then(getVotes)
-    
-})
-// put in zeros for both values
-// axios.post('http://circuslabs.net:3000/data/yoda', {
-//   type: 'number',
-//   action: '=',
-//   value: 13
-// })
-// axios.post('http://circuslabs.net:3000/data/vader', {
-//   type: 'number',
-//   action: '=',
-//   value: 17
-// })
 
 
 
